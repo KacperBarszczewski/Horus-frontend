919da78c855a18319ca9aa8a9f43607c34c1dd04
KacperB <kacpero.barszczewski@gmail.com>
2025-05-06 18:26:17 +0200

    feat: implement employee tree component with dynamic rendering and subtree retrieval

--- src/app/components/employee-tree/employee-tree.component.ts
+++ src/app/components/employee-tree/employee-tree.component.ts
@@ -1,23 +1,25 @@
 import { Component, Input, SimpleChanges } from '@angular/core';
 import { Employee } from '../../models/employee';
 import { EmployeeService } from '../../services/employee.service';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-employee-tree',
   templateUrl: './employee-tree.component.html',
   styleUrls: ['./employee-tree.component.css'],
+  imports: [CommonModule],
   standalone: true
 })
 export class EmployeeTreeComponent {
   @Input() selectedEmployee: Employee | null = null;
 
+  employeeSubtree: Employee[] | null = [];
+
   constructor(private employeeService: EmployeeService) { }
 
-  ngOnChanges(changes: SimpleChanges): void {
+  ngOnChanges(changes: SimpleChanges) {
     if (changes['selectedEmployee'] && this.selectedEmployee) {
-
-      const result = this.employeeService.findEmployeeSubtree(this.selectedEmployee);
-      console.log(result);
+      this.employeeSubtree = this.employeeService.findEmployeeSubtree(this.selectedEmployee);
     }
   }
 
